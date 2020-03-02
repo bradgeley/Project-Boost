6e36e0f4edbe851d4eed1b275d2eb6ebaadc0bdb
Bradley Christensen <49649546+bradjc95certitude@users.noreply.github.com>
2020-03-02 13:22:03 -0600

    boosting and turning implemented

---
+++ Assets/Rocket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocket;
+    enum Direction { Left, Right };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocket = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            Turn(Direction.Left);
+        }
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            Turn(Direction.Right);
+        }
+    }
+
+    private void Turn(Direction d)
+    {
+        switch (d)
+        {
+            case Direction.Left:
+                transform.Rotate(Vector3.forward);
+                break;
+            case Direction.Right:
+                transform.Rotate(Vector3.back);
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        rocket.AddRelativeForce(Vector3.up);
+    }
+}
