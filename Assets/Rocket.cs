48f28059ade95cce73089701474a0e4023d72309
Bradley Christensen <49649546+bradjc95certitude@users.noreply.github.com>
2020-03-02 15:20:28 -0600

    Level 1 done

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,52 +5,73 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    //Game Data
+    const float MAX_FUEL = 100;
+
+    //Global Variables
+    [SerializeField] float thrustPower = 1750f;
+    [SerializeField] float rotationPower = 250f;
+    [SerializeField] float fuel;
+
+    //Game Objects
     Rigidbody rocket;
+    AudioSource thrust;
     enum Direction { Left, Right };
 
     // Start is called before the first frame update
     void Start()
     {
         rocket = GetComponent<Rigidbody>();
+        thrust = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        processInput();
+        ManageThrust();
+        ManageRotation();
     }
 
-    private void processInput()
+    private void ManageRotation()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Thrust();
-        }
+        rocket.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            Turn(Direction.Left);
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationPower);
         }
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            Turn(Direction.Right);
+            transform.Rotate(Vector3.back * Time.deltaTime * rotationPower);
         }
+        rocket.freezeRotation = false; //resume physics
     }
 
-    private void Turn(Direction d)
+    private void ManageThrust()
     {
-        switch (d)
+        if (Input.GetKey(KeyCode.Space))
         {
-            case Direction.Left:
-                transform.Rotate(Vector3.forward);
-                break;
-            case Direction.Right:
-                transform.Rotate(Vector3.back);
-                break;
+            rocket.AddRelativeForce(Vector3.up * Time.deltaTime * thrustPower);
+            if (!thrust.isPlaying)
+            {
+                thrust.Play();
+            }
+        }
+        else
+        {
+            thrust.Stop();
         }
     }
 
-    private void Thrust()
+    void OnCollisionEnter(Collision collision)
     {
-        rocket.AddRelativeForce(Vector3.up);
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("DEAD");
+                break;
+        }
     }
 }
